c8e6451e4c979cd2f8b52234e082dea807f0840e
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2024-07-18 15:22:52 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :fast_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :fast_name_kana, presence: true
          validates :date_of_birth, presence: true
+
+         validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'は有効なメールアドレスを入力してください' }
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
+         validates :password_confirmation, presence: true
+         validates :password, confirmation: { case_sensitive: true }
+
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください' }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください' }
+
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+end
 end
--- db/migrate/20240717125817_add_details_to_users.rb
+++ db/migrate/20240717125817_add_details_to_users.rb
@@ -6,5 +6,15 @@ class AddDetailsToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :last_name_kana, :string
     add_column :users, :fast_name_kana, :string
     add_column :users, :date_of_birth, :date
+
+    VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
+
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'は有効なメールアドレスを入力してください' }
+
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+    validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
+    validates :password_confirmation, presence: true
+    validates :password, confirmation: { case_sensitive: true }
   end
 end
