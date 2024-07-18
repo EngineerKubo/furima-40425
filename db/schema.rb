3075f2183e9ab3743a222b8142bd4dcb8ba3dc1c
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2024-07-19 03:33:25 +0900

    Userのモデル単体テスト実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_07_17_125817) do
-  create_table "products", charset: "utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "text", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,9 +21,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_17_125817) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.string "last_name"
-    t.string "fast_name"
+    t.string "first_name"
     t.string "last_name_kana"
-    t.string "fast_name_kana"
+    t.string "first_name_kana"
     t.date "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
