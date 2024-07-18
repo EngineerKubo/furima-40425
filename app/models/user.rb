8c4dd3696d3ddd58d8639e4c9ab807d40d7783e0
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2024-07-18 16:21:22 +0900

    バリデーション設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+         VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龠々ー]+\z/
+         VALID_NAME_KANA_REGEX = /\A[ァ-ヶー]+\z/
+         validates :nickname, presence: true
+         validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'は有効なメールアドレスを入力してください' }
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }, confirmation: true
+         validates :password_confirmation, presence: true
+         validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください' }
+         validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください' }
+         validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+         validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+         validates :date_of_birth, presence: true
 end
--- db/migrate/20240717125817_add_details_to_users.rb
+++ db/migrate/20240717125817_add_details_to_users.rb
@@ -2,9 +2,9 @@ class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :nickname, :string
     add_column :users, :last_name, :string
-    add_column :users, :fast_name, :string
+    add_column :users, :first_name, :string
     add_column :users, :last_name_kana, :string
-    add_column :users, :fast_name_kana, :string
+    add_column :users, :first_name_kana, :string
     add_column :users, :date_of_birth, :date
   end
 end
