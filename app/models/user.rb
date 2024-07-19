3542d0ed3884a958ed5c29974de6113ee89acac9
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2024-07-19 19:05:37 +0900

    指摘事項の修正2回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-         VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龠々ー]+\z/
+         VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
          VALID_NAME_KANA_REGEX = /\A[ァ-ヶー]+\z/
          validates :nickname, presence: true
          validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }, confirmation: true
