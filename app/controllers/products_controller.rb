ff89e6511d22dcabd29d0b05ce402601fa5caf1a
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2024-07-31 15:59:01 +0900

    編集ページの変更処理の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :set_product, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @products = Product.order("created_at DESC")
@@ -28,10 +31,24 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-    @product.update(product_params)
+    if @product.update(product_params)
+    redirect_to product_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def correct_user
+    unless current_user == @product.user
+      redirect_to products_path, alert: '他のユーザーの投稿を編集することはできません。'
+    end
+  end
 
   def product_params
     params.require(:product).permit(:product_name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_shipping_id, :price, :image).merge(user_id: current_user.id)
