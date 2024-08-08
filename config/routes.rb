c052a5e8e78e3442dc000ac11f77610e7c815e3e
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2024-08-08 21:10:06 +0900

    エラー箇所修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'products/index'
-  get 'products/new'
   root to: 'products#index'
   resources :products do
     resources :orders, only: [:index, :create]
